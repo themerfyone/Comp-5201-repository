ad96795f0985676418f44e9b254d404b5eda0ec7
Joel Collecutt <9977625@student.toiohomai.ac.nz>
2018-07-25 16:39:17 +1200

    first

---
+++ app1/app1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace app1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //writeline outputs to the console
+            Console.WriteLine("Hello World");
+            //readline waits for input.
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
